9ca0f31e9a90fe657ee5c218c285cd72159ff870
Ruchita Gosavi <ruchigosavi10@gmail.com>
2024-02-12 20:14:53 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+function App() {
+  return (
+    <main className="h-screen bg-zinc-950">
+      <h1 className="text-zinc-200 text-lg">Haikuist</h1>
+    </main>
+  );
+}
+
+export default App;
