a9c94a7fbe295ff713a9be0daa678d3386e6fa0a
Ruchita Gosavi <ruchigosavi10@gmail.com>
2024-04-18 00:04:21 +0530

    fix: main content shrinking in Profile and Create pages

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout() {
         </div>
         <Navbar />
       </header>
-      <main className="min-h-screen min-w-0 px-6 pt-3 border-fgColor/15 sm:border-r">
+      <main className="flex-1 min-h-screen min-w-0 px-6 pt-3 border-fgColor/15 sm:border-r">
         <Outlet />
       </main>
       <aside className="hidden md:block min-w-72 max-w-72 h-screen sticky top-0">
