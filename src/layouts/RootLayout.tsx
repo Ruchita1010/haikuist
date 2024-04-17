b25d285b9459b1189a9fa7ea4dd90018d8e0e4d9
Ruchita Gosavi <ruchigosavi10@gmail.com>
2024-04-17 23:42:46 +0530

    fix: right sidebar not sticking to top

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -8,21 +8,21 @@ export default function RootLayout() {
   return session === null ? (
     <Navigate to="/signin" replace />
   ) : (
-    <div className="min-h-screen flex flex-col-reverse md:flex-row transition-colors duration-300 ease-in-out">
-      <header className="sticky bottom-0 lg:w-72 md:max-lg:w-24 md:px-4 border-t border-fgColor/15 md:border-t-0 md:border-r">
-        <div className="hidden md:block text-center my-4 lg:mx-4">
+    <div className="max-w-[90rem] mx-auto flex flex-col-reverse md:flex-row transition-colors duration-300 ease-in-out">
+      <header className="sticky bottom-0 md:top-0 md:h-screen lg:min-w-56 md:px-4 border-t md:border-t-0 md:border-r border-fgColor/15">
+        <div className="hidden md:block text-center my-4">
           <Link to="/home">
             <h1>Haikuist</h1>
           </Link>
         </div>
         <Navbar />
       </header>
-      <main className="w-full h-screen flex overflow-y-auto">
-        <div className="w-full p-6 border-fgColor/15 sm:max-lg:w-7/12 lg:w-4/6 sm:border-r">
-          <Outlet />
-        </div>
-        <div className="hidden sm:block">Today's Pick</div>
+      <main className="min-h-screen min-w-0 px-6 pt-3 border-fgColor/15 sm:border-r">
+        <Outlet />
       </main>
+      <aside className="hidden md:block min-w-72 max-w-72 h-screen sticky top-0">
+        <h1>Today's Pick</h1>
+      </aside>
     </div>
   );
 }
